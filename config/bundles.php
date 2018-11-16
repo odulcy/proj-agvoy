b4388b240c8612752f956a4e6def03afda1ae292
Olivier DULCY <olivier.dulcy@telecom-sudparis.eu>
2018-11-16 12:45:55 +0100

    KnpMenu added

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
 ];
