cc8049488291406c40b8d19000c2b873d8a94e2d
Olivier DULCY <olivier.dulcy@telecom-sudparis.eu>
2018-11-27 00:50:02 +0100

    New DataFixtures

--- src/DataFixtures/LoadProgrammationCircuitData.php
+++ src/DataFixtures/LoadProgrammationCircuitData.php
@@ -10,18 +10,72 @@ class LoadProgrammationCircuitData extends Fixture
 {
 	public function load(ObjectManager $manager)
         {       
-
                 $circuit = $this->getReference('andalousie-circuit');
 
                 $programmationCircuit = new ProgrammationCircuit();
                 $programmationCircuit->setDateDepart(new \DateTime('2018-08-01'));
 		$programmationCircuit->setNombrePersonnes(100);
-                $programmationCircuit->setPrix(2500);
+                $programmationCircuit->setPrix(1500);
                 $circuit->addProgrammationCircuit($programmationCircuit);
 		$manager->persist($programmationCircuit);
 		
 		$this->addReference('andalousie-programmationCircuit', $programmationCircuit);
 		
+                $circuit = $this->getReference('idf-circuit');
+
+                $programmationCircuit = new ProgrammationCircuit();
+                $programmationCircuit->setDateDepart(new \DateTime('2018-12-01'));
+		$programmationCircuit->setNombrePersonnes(4);
+                $programmationCircuit->setPrix(2300);
+                $circuit->addProgrammationCircuit($programmationCircuit);
+		$manager->persist($programmationCircuit);
+		
+		$this->addReference('idf-programmationCircuit', $programmationCircuit);
+
+                $circuit = $this->getReference('italie-circuit');
+
+                $programmationCircuit = new ProgrammationCircuit();
+                $programmationCircuit->setDateDepart(new \DateTime('2018-11-09'));
+		$programmationCircuit->setNombrePersonnes(100);
+                $programmationCircuit->setPrix(2500);
+
+		$manager->persist($programmationCircuit);
+		
+		$this->addReference('italie-programmationCircuit', $programmationCircuit);
+
+                $circuit = $this->getReference('vietnam-circuit');
+
+                $programmationCircuit = new ProgrammationCircuit();
+                $programmationCircuit->setDateDepart(new \DateTime('2019-01-06'));
+		$programmationCircuit->setNombrePersonnes(100);
+                $programmationCircuit->setPrix(2500);
+                $circuit->addProgrammationCircuit($programmationCircuit);
+		$manager->persist($programmationCircuit);
+
+		$this->addReference('vietnam-programmationCircuit', $programmationCircuit);
+
+                $circuit = $this->getReference('andalousie-circuit');
+
+                $programmationCircuit = new ProgrammationCircuit();
+                $programmationCircuit->setDateDepart(new \DateTime('2019-02-03'));
+		$programmationCircuit->setNombrePersonnes(100);
+                $programmationCircuit->setPrix(2500);
+                $circuit->addProgrammationCircuit($programmationCircuit);
+		$manager->persist($programmationCircuit);
+		
+		$this->addReference('andalousie-programmationCircuit2', $programmationCircuit);
+
+                $circuit = $this->getReference('idf-circuit');
+
+                $programmationCircuit = new ProgrammationCircuit();
+                $programmationCircuit->setDateDepart(new \DateTime('2019-06-04'));
+		$programmationCircuit->setNombrePersonnes(100);
+                $programmationCircuit->setPrix(2500);
+                $circuit->addProgrammationCircuit($programmationCircuit);
+		$manager->persist($programmationCircuit);
+		
+		$this->addReference('idf-programmationCircuit2', $programmationCircuit);
+
 		$manager->flush();
 	}
 	
