aa086c12d7aa3421ce3423e252e9f641d41488ad
Olivier DULCY <olivier.dulcy@telecom-sudparis.eu>
2018-11-26 14:53:27 +0100

    vich_uploader bundle added.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
